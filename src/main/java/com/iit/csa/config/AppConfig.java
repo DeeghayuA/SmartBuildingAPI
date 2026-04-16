efedd590158f0688a3a441563333be204e46647d
DeeghayuA <deeghayuarandara12@gmail.com>
2026-04-16 23:29:56 +0530

    Create AppConfig.java
    create new package "com.iit.csa.config" and create the AppConfig Class

---
+++ src/main/java/com/iit/csa/config/AppConfig.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.iit.csa.config;
+
+/**
+ *
+ * @author deegh
+ */
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api/v1")
+
+public class AppConfig extends Application {
+    
+}
