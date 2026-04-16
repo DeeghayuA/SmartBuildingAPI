b277d1eaf83e07863043977e14ef91a47a61f786
DeeghayuA <deeghayuarandara12@gmail.com>
2026-04-17 00:05:46 +0530

    create the Sensors class

--- src/main/java/com/iit/csa/models/Sensor.java
+++ src/main/java/com/iit/csa/models/Sensor.java
@@ -8,26 +8,27 @@ package com.iit.csa.models;
  *
  * @author deegh
  */
-public class Room {
+public class Sensor {
     private int id;
+    private int roomId;
     private String name;
-    private String floor;
+    private String type; 
+
+    public Sensor() {} //for JSON
+    public Sensor(int id, int roomId, String name, String type) { this.id = id; this.roomId = roomId; this.name = name; this.type = type; }
+
     
-    public Room(int id, String name, String floor) {
-        this.id = id;
-        this.name = name;
-        this.floor = floor;
-    }    
-    public Room() {} //for JSON
     
     
     public int getId() { return id; }
     public void setId(int id) { this.id = id; }
     
+    public int getRoomId() { return roomId; }
+    public void setRoomId(int roomId) { this.roomId = roomId; }
+    
     public String getName() { return name; }
     public void setName(String name) { this.name = name; }
     
-    public String getFloor() { return floor; }
-    public void setFloor(String floor) { this.floor = floor; }
-    
+    public String getType() { return type; }
+    public void setType(String type) { this.type = type; }
 }
